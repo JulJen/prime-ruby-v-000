fd22cc1caa6ff40d1c85df84305e1f1faca790d3
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-19 22:53:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -11,22 +11,3 @@ def prime?(n)
        return true
    end
 end
-# end
-#   # if number.odd? && (number - 1) > 2
-#   #   return true
-#   if number <= 1
-#     return false
-#
-# # herewego = Numeric.new
-# # (1..10000).each do
-# #    |container|
-# #    (2..(Math.sqrt(container).ceil)).each do |x|
-# #       herewego = 1
-# #       if (container.divmod(x)[1] == 0)
-# #          herewego = 0
-# #          break
-# #       end
-# #    end
-# #    print "#{container}\," unless (herewego == 0)
-#   end
-# end
