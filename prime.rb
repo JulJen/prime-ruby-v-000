9c38e99b9cbc4199b085cceb1189c56739373b77
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-19 22:46:07 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -11,8 +11,7 @@ def prime?(number)
 
   if number.odd? && (number - 1) > 2
     return true
-  elsif number.find?(1763)
-    return false
+
     # odd? && number % 3 != 0
     # number % 2 == 0 && number % 3 == 0
   # true
