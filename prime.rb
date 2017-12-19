573a76c165ab76d1032c43a722b5d095535419b6
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-19 22:14:08 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -8,7 +8,7 @@ def prime?(number)
   #     return false
   #   end
   # end
-    (2..(number - 1)).each do |n|
+    (number - 1).each do |n|
       return false if number % n == 0
     end
     true
