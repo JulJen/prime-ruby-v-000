f95f359cc7a5b462e50b6ca92a6a66ac2700cd0a
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-19 22:56:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,12 @@
 
 def prime?(given)
   prime = []
-   1.upto(given){|number| prime << number if (given % number == 0)}
-   if given <= 1
-       return false
-   elsif prime.count > 2
-       return false
+  1.upto(given){|number| prime << number if (given % number == 0)}
+    if given <= 1
+      return false
+    elsif prime.count > 2
+      return false
    else
-       return true
+     return true
    end
-end
+ end
