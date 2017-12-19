b146e0d764cdf2490cfd65e1f0513727d4c62827
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-19 22:55:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 
 
-def prime?(n)
-  one_and_itself = []
-   1.upto(n){|number| one_and_itself << number if (n % number == 0)}
-   if n <= 1
+def prime?(givem)
+  prime = []
+   1.upto(given){|number| prime << number if (given % number == 0)}
+   if given <= 1
        return false
-   elsif one_and_itself.count > 2
+   elsif prime.count > 2
        return false
    else
        return true
