c0cbf2aef3d8010c23e985a4be0082702981d30e
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-19 22:13:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,33 @@
-# Add  code here!
+
+
+def prime?(number)
+  # number.each {|odd_number| odd_number.odd?}
+  #   if number.include?(number.odd?)
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
+    (2..(num - 1)).each do |n|
+      return false if num % n == 0
+    end
+    true
+  end
+end
+  # number.each_with_index|test|
+  # if number <= 1 || number.even?
+  #   false
+  # elsif number.odd? || number % 2 == 0
+  # # if number.odd?
+  #   return true
+  # elsif 1 / number < 0
+  #   return false
+  # elsif number % 1 == 0
+  #   return false
+  #
+  # # when number.(-1)
+  # #   return false
+  # else
+  #   return false
+#   end
+# end
