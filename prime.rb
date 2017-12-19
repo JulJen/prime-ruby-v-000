bc688c0dbc49a1a7ac1390d6b811b8e9a0aee214
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-19 22:51:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,25 @@
 
 
 def prime?(number)
-  # if number.odd? && (number - 1) > 2
-  #   return true
-  if number <= 1
-    return false
-
-# herewego = Numeric.new
-# (1..10000).each do
-#    |container|
-#    (2..(Math.sqrt(container).ceil)).each do |x|
-#       herewego = 1
-#       if (container.divmod(x)[1] == 0)
-#          herewego = 0
-#          break
-#       end
-#    end
-#    print "#{container}\," unless (herewego == 0)
-  end
+  return false if [0, 1].include?(number)
+(2...number).none? { |n| number % n == 0}
 end
+end
+#   # if number.odd? && (number - 1) > 2
+#   #   return true
+#   if number <= 1
+#     return false
+#
+# # herewego = Numeric.new
+# # (1..10000).each do
+# #    |container|
+# #    (2..(Math.sqrt(container).ceil)).each do |x|
+# #       herewego = 1
+# #       if (container.divmod(x)[1] == 0)
+# #          herewego = 0
+# #          break
+# #       end
+# #    end
+# #    print "#{container}\," unless (herewego == 0)
+#   end
+# end
